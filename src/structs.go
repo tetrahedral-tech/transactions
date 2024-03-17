b44055a4e6aef75813cb9f3dae08de65297d80b8
CelestialCrafter <celestialcraftermc@gmail.com>
2024-03-17 18:00:09 -0500

    i was wrong - casing is hard - reverts most of last commit

--- src/structs.go
+++ src/structs.go
@@ -5,37 +5,37 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type TradeType int8
 
 const (
-	buy       TradeType = 0
-	sell      TradeType = 1
-	no_action TradeType = 2
+	Buy       TradeType = 0
+	Sell      TradeType = 1
+	NoAction  TradeType = 2
 )
 
 type AlgorithmSignal struct {
-	algorithm string `json:"algorithm"`
-	amount    int    `json:"amount"`
-	signal    string `json:"signal"`
+	Algorithm string `json:"algorithm"`
+	Amount    int    `json:"amount"`
+	Signal    string `json:"signal"`
 }
 
 type Worth struct {
-	id        string  `bson:"_id"`
-	timestamp int     `bson:"timestamp"`
-	value     float64 `bson:"value"`
+	ID        string  `bson:"_id"`
+	Timestamp int     `bson:"timestamp"`
+	Value     float64 `bson:"value"`
 }
 
 type Status struct {
-	name string `bson:"name"`
-	time int    `bson:"time"`
+	Name string `bson:"name"`
+	Time int    `bson:"time"`
 }
 
 type Bot struct {
-	id                  primitive.ObjectID `bson:"_id"`
-	owner               primitive.ObjectID `bson:"owner"`
-	algorithm           primitive.ObjectID `bson:"algorithm"`
-	strengthToUsd       int                `bson:"strengthToUSD"`
-	encryptedPrivateKey string             `bson:"encryptedPrivateKey"`
-	worth               []Worth            `bson:"worth"`
-	status              Status             `bson:"status"`
-	pair                []string           `bson:"pair"`
-	provider            string             `bson:"provider"`
-	interval            int                `bson:"interval"`
+	ID                  primitive.ObjectID `bson:"_id"`
+	Owner               primitive.ObjectID `bson:"owner"`
+	Algorithm           primitive.ObjectID `bson:"algorithm"`
+	StrengthToUsd       int                `bson:"strengthToUSD"`
+	EncryptedPrivateKey string             `bson:"encryptedPrivateKey"`
+	Worth               []Worth            `bson:"worth"`
+	Status              Status             `bson:"status"`
+	Pair                []string           `bson:"pair"`
+	Provider            string             `bson:"provider"`
+	Interval            int                `bson:"interval"`
 }
